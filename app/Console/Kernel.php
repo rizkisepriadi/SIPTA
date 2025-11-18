c84ba6429d05714b7a2e3290fa9d842077876bb1
Haruki <jimmymaulana01@gmail.com>
2025-11-18 20:41:44 +0700

    kernel commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
     }
 
     /**
@@ -27,6 +26,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require_once base_path('routes/console.php');
     }
 }
